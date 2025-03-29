2f5e19703cc5f062d577cb0213fb0ef838ed9353
Prashant Rathod <57872227+prashant0706@users.noreply.github.com>
2025-03-29 17:44:24 +0000

    App.Js imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { BrowserRouter } from 'react-router-dom'
 
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech , Works, StarsCanvas } from './components';
 
 const App = () =>{
 
   return (
-    <div>3D developer portfolio</div>
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
